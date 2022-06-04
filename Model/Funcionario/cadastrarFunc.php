ce6a2f949d68b6b6197977db43cfce1879096d22
chacheta <starkstory6@gmail.com>
2022-06-04 01:54:15 -0300

    cadastrarFunc funcionando

--- Model/Funcionario/cadastrarFunc.php
+++ Model/Funcionario/cadastrarFunc.php
@@ -6,7 +6,7 @@
     ob_start();
 
     //Incluir a conexao com BD
-    include_once "../../Conexao.php"; 
+    include_once "../../Banco/Conexao.php"; 
     
     //Receber os dados do formulario
     $dados = filter_input_array(INPUT_POST, FILTER_DEFAULT);
@@ -14,13 +14,13 @@
     
     if(isset($dados['btnCadastrar'])){
         $foto = $_FILES['imagem'];
-        $dados['comparaSenhas'] = md5($dados['Confirmasenha']);
-        $dados['senhas'] = md5($dados['senha']);
+        $comparaSenha = md5($dados['Confirmasenha']);
+        $senha = md5($dados['senha']);
         $error = array();
         // Verifica se o arquivo é uma imagem
         if(!preg_match("/^image\/(pjpeg|jpeg|png|gif|bmp)$/", $foto["type"])){
             $_SESSION['msg'] = '<div class="alert alert-danger" role="alert"><b>Isso não é uma imagem &#128552;</b></div>';
-            //header("Location:../index.php");
+            header("Location:../../Views/Funcionario/cadastrarFunc.php");
         } 
         if (count($error) == 0) {
                 // Pega extensão da imagem
@@ -28,16 +28,16 @@
                 // Gera um nome único para a imagem
                 $nome_imagem = md5(uniqid(time())) . "." . $ext[1];
                 // Caminho de onde ficará a imagem
-                $caminho_imagem = "../../Views/Funcionario/images/FotoPerfil/" . $nome_imagem;
+                $caminho_imagem = "../../Views/Funcionario/assets/images/FotoPerfil/" . $nome_imagem;
                 // Faz o upload da imagem para seu respectivo caminho
                 if(move_uploaded_file($foto["tmp_name"], $caminho_imagem)){
                     $query_usuario = "INSERT INTO usuario 
                     (nome,tipo,senha,usuario,genero,cpf,salario,cargaHoraria,dica,foto) VALUES
                     (:nome , :tipo, :senha ,:usuario, :genero , :cpf, :salario, :cargaHoraria, :dica,:foto)";
-                    $cad_usuario = $conn->prepare($query_usuario);
+                    $cad_usuario = $connPDO->prepare($query_usuario);
                     $cad_usuario->bindParam(':nome', $dados['nome'], PDO::PARAM_STR);
                     $cad_usuario->bindParam(':tipo', $dados['permissao'], PDO::PARAM_STR);
-                    $cad_usuario->bindParam(':senha',md5($dados['senha']), PDO::PARAM_STR);
+                    $cad_usuario->bindParam(':senha',$senha, PDO::PARAM_STR);
                     $cad_usuario->bindParam(':usuario', $dados['usuario'], PDO::PARAM_STR);
                     $cad_usuario->bindParam(':genero', $dados['genero'], PDO::PARAM_STR);
                     $cad_usuario->bindParam(':cpf', $dados['cpf'], PDO::PARAM_STR);
@@ -48,12 +48,12 @@
                     $cad_usuario->execute();
                     //var_dump($conn->lastInsertId());
                     //Recuperar o ultimo id inserido
-                    $id_usuario = $conn->lastInsertId();
+                    $id_usuario = $connPDO->lastInsertId();
         
                     $query_endereco= "INSERT INTO endereco 
                                 (cep,logradouro,bairro,cidade,estado,complemento,codEndereco,numero) VALUES 
                                 (:cep, :logradouro, :bairro, :cidade,:estado,:complemento,:id_usuario,:numero)";
-                    $cad_endereco = $conn->prepare($query_endereco);
+                    $cad_endereco = $connPDO->prepare($query_endereco);
                     $cad_endereco->bindParam(':cep', $dados['cep'], PDO::PARAM_STR);
                     $cad_endereco->bindParam(':logradouro', $dados['logradouro'], PDO::PARAM_STR);
                     $cad_endereco->bindParam(':bairro', $dados['bairro'], PDO::PARAM_STR);
@@ -67,7 +67,7 @@
                     $query_telefone = "INSERT INTO telefone
                                     (ddd,telefone,tipo,codTelefone) VALUES
                                     (:ddd,:telefone,:tipo,:id_usuario)";
-                    $cad_telefone = $conn->prepare($query_telefone);
+                    $cad_telefone = $connPDO->prepare($query_telefone);
                     $cad_telefone->bindParam(':ddd', $dados['ddd'], PDO::PARAM_STR);
                     $cad_telefone->bindParam(':telefone', $dados['telefone'], PDO::PARAM_STR);
                     $cad_telefone->bindParam(':tipo', $dados['tipoTelefone'], PDO::PARAM_STR);
@@ -76,7 +76,7 @@
         
                     //Criar a variavel global para salvar a mensagem de sucesso
                     $_SESSION['msg'] = '<div class="alert alert-success" role="alert">Usuário Cadastrado com sucesso &#128526</div>';
-                    //header("Location:../index.php");
+                    header("Location:../../Views/Funcionario/cadastrarFunc.php");
                 }
             }
         }
