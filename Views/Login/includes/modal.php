33c62a8c990399aff8e60b601b606f0db9071b69
chacheta <starkstory6@gmail.com>
2022-06-12 23:11:24 -0300

    a

---
+++ Views/Login/includes/modal.php
@@ -0,0 +1,23 @@
+<!-- The Modal -->
+<div class="modal fade" id="myModal">
+<div class="modal-dialog modal-dialog-centered">
+<div class="modal-content">
+
+    <!-- Modal Header -->
+    <div class="modal-header">
+    <h4 class="modal-title">Atenção</h4>
+    <button type="button" class="close" data-dismiss="modal">&times;</button>
+    </div>
+    
+    <!-- Modal body -->
+    <div class="modal-body">
+    <b>Dados invalidos</b>
+    </div>
+    
+    <!-- Modal footer -->
+    <div class="modal-footer">
+    <button type="button" onclick="window.location.href=esqueceuSenha.php?erro" class="btn btn-danger" data-dismiss="modal">Tentar novamente</button>
+    </div>
+    
+</div>
+</div>';
